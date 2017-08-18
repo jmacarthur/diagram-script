fea4a803c0f218c307858b88226efa480f957df5
Jim MacArthur <jim@mode7.co.uk>
2017-08-18 17:10:12 +0100

    Remove newline at start of output

--- header.py
+++ header.py
@@ -1,5 +1,4 @@
-svg_header = '''
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+svg_header = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <svg
   xmlns:svg="http://www.w3.org/2000/svg"
   xmlns="http://www.w3.org/2000/svg"
